569a8da7f5b3df8f6123dc8e2402195a57d426e1
Kononkov1998 <kononkov98@mail.ru>
2020-08-14 17:19:29 +0700

    Фикс обработки ошибки

--- backend/games/utils.py
+++ backend/games/utils.py
@@ -1,5 +1,5 @@
+from django.core.exceptions import ValidationError
 from rest_framework import status
-from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 
 from games.models import UserGame, Game
