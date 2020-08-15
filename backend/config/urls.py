e66a40825a1f8e3cdef3a53d72244409ee824a0f
Kononkov1998 <kononkov98@mail.ru>
2020-08-15 16:25:51 +0700

    Снес админку

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
 from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
@@ -17,7 +16,6 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('games/', include('games.urls')),
 ]
--- backend/users/models.py
+++ backend/users/models.py
@@ -25,7 +25,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_('email address'), unique=True, error_messages={
         'unique': _("A user with that email already exists."),
     }, )
-    is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
     date_joined = models.DateTimeField(default=timezone.now)
 
