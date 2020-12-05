f1d19c86f2b50fa4c4d9e5ebbfcaae750147ad78
Kononkov1998 <kononkov98@mail.ru>
2020-12-06 02:37:12 +0700

    Фикс сигналов

--- backend/users/signals.py
+++ backend/users/signals.py
@@ -20,7 +20,6 @@ def create_log(instance, **kwargs):
     for field in fields:
         if field in user_log_dict and (not old_fields or fields[field] != old_fields[field]):
             action_type = field
-            if fields[field] is not None:
-                action_result = fields[field]
-                UserLog.objects.create(user=instance.user, followed_user=instance.followed_user,
-                                       action_type=action_type, action_result=action_result)
+            action_result = fields[field]
+            UserLog.objects.create(user=instance.user, followed_user=instance.followed_user,
+                                   action_type=action_type, action_result=action_result)
