4b9d56700da150959278e2dc5f512e0091d7dd33
Kononkov1998 <kononkov98@mail.ru>
2021-02-24 18:54:45 +0700

    Перевёл ошибку авторизации

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.password_validation import validate_password
+from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
@@ -126,6 +127,10 @@ class UserInfoSerializer(serializers.ModelSerializer):
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    default_error_messages = {
+        'no_active_account': _('Неверный логин или пароль.')
+    }
+
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
