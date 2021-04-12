7286ce97b0585acc27b665923689cde529974e3f
vkoktashev <vladislav54415@gmail.com>
2021-04-12 17:42:00 +0700

    фикс поиска

--- frontend/src/components/Common/SearchInput.js
+++ frontend/src/components/Common/SearchInput.js
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from "react";
+import { observer } from "mobx-react";
 import SearchStore from "../../store/SearchStore";
 import { useHistory } from "react-router-dom";
+import { MDBIcon } from "mdbreact";
 
-function SearchInput({ onSubmit }) {
+const SearchInput = observer(({ onSubmit }) => {
 	const [query, setQuery] = useState("");
 	let history = useHistory();
 	const { searchHints, hints } = SearchStore;
 
-	useEffect(() => {}, [hints]);
+	useEffect(() => {
+		console.log("ЖОПА");
+	}, [hints]);
 
 	return (
 		<form onSubmit={onSubmit}>
@@ -25,7 +29,7 @@ function SearchInput({ onSubmit }) {
 				/>
 				<div className='searchHints' hidden={query === "" || !(hints.games.length > 0 || hints.movies.length > 0 || hints.shows.length > 0)}>
 					<div hidden={!hints.games.length > 0}>
-						Игры:
+						<MDBIcon icon='gamepad' />
 						{hints.games.map((hint) => (
 							<a
 								key={hint.rawg_slug}
@@ -36,13 +40,14 @@ function SearchInput({ onSubmit }) {
 									e.preventDefault();
 									setQuery("");
 								}}>
-								{/*<img src={hint.rawg_backdrop_path} />*/}
+								{<img src={hint.rawg_backdrop_path} alt={""} />}
 								{hint.rawg_name}
+								<div>{hint?.rawg_release_date?.substr(0, 4)}</div>
 							</a>
 						))}
 					</div>
 					<div hidden={!hints.movies.length > 0}>
-						Фильмы:
+						<MDBIcon icon='film' />
 						{hints.movies.map((hint) => (
 							<a
 								key={hint.rawg_slug}
@@ -53,12 +58,14 @@ function SearchInput({ onSubmit }) {
 									e.preventDefault();
 									setQuery("");
 								}}>
+								{<img src={hint.tmdb_backdrop_path} alt={""} />}
 								{hint.tmdb_name}
+								<div>{hint?.tmdb_release_date?.substr(0, 4)}</div>
 							</a>
 						))}
 					</div>
 					<div hidden={!hints.shows.length > 0}>
-						Сериалы:
+						<MDBIcon icon='tv' />
 						{hints.shows.map((hint) => (
 							<a
 								key={hint.rawg_slug}
@@ -69,7 +76,9 @@ function SearchInput({ onSubmit }) {
 									e.preventDefault();
 									setQuery("");
 								}}>
+								{<img src={hint.tmdb_backdrop_path} alt={""} />}
 								{hint.tmdb_name}
+								<div>{hint?.tmdb_release_date?.substr(0, 4)}</div>
 							</a>
 						))}
 					</div>
@@ -77,6 +86,6 @@ function SearchInput({ onSubmit }) {
 			</div>
 		</form>
 	);
-}
+});
 
 export default SearchInput;
