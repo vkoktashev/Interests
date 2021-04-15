fe2127970b0540de074b507450520e9bd1251564
vkoktashev <vladislav54415@gmail.com>
2021-04-15 22:20:11 +0700

    фикс подтверждения пароля

--- frontend/src/components/ConfirmPasswordPage.js
+++ frontend/src/components/ConfirmPasswordPage.js
@@ -16,39 +16,44 @@ const ConfirmPasswordPage = observer((props) => {
 	let token = params.get("token");
 
 	return (
-		<div className='bg'>
-			<form
-				onSubmit={(event) => {
-					event.preventDefault();
-					confirmPassword(token, password);
-				}}
-				className='confirmPasswordForm'>
-				<p className='h4'>Обновить пароль</p>
-				<p className='note note-danger' hidden={!confirmPasswordState.startsWith("error:")}>
-					{confirmPasswordState}
-				</p>
-				<p className='note note-success' hidden={confirmPasswordState !== "done"}>
-					Пароль обновлен!
-				</p>
+		<div>
+			<div className='bg' />
+			<div className='contentPage'>
+				<div className='contentBody'>
+					<form
+						onSubmit={(event) => {
+							event.preventDefault();
+							confirmPassword(token, password);
+						}}
+						className='confirmPasswordForm'>
+						<p className='h4'>Обновить пароль</p>
+						<p className='note note-danger' hidden={!confirmPasswordState.startsWith("error:")}>
+							{confirmPasswordState}
+						</p>
+						<p className='note note-success' hidden={confirmPasswordState !== "done"}>
+							Пароль обновлен!
+						</p>
 
-				<label htmlFor='passwordInput' className='confirmPasswordLabel'>
-					Новый пароль
-				</label>
-				<br />
-				<input type='password' id='passwordInput' className='confirmPasswordInput' value={password} onChange={(event) => setPassword(event.target.value)} />
-				<br />
+						<label htmlFor='passwordInput' className='confirmPasswordLabel'>
+							Новый пароль
+						</label>
+						<br />
+						<input type='password' id='passwordInput' className='confirmPasswordInput' value={password} onChange={(event) => setPassword(event.target.value)} />
+						<br />
 
-				<label htmlFor='passwordConfurmInput' className='confirmPasswordLabel'>
-					Подтверждение пароля
-				</label>
-				<br />
-				<input type='password' id='passwordConfurmInput' className='confirmPasswordInput' value={passwordConfirm} onChange={(event) => setPasswordConfirm(event.target.value)} />
-				<br />
+						<label htmlFor='passwordConfurmInput' className='confirmPasswordLabel'>
+							Подтверждение пароля
+						</label>
+						<br />
+						<input type='password' id='passwordConfurmInput' className='confirmPasswordInput' value={passwordConfirm} onChange={(event) => setPasswordConfirm(event.target.value)} />
+						<br />
 
-				<button type='submit' className='confirmPasswordButton' disabled={(password !== passwordConfirm) | (password.length < 1)}>
-					Обновить
-				</button>
-			</form>
+						<button type='submit' className='confirmPasswordButton' disabled={(password !== passwordConfirm) | (password.length < 1)}>
+							Обновить
+						</button>
+					</form>
+				</div>
+			</div>
 		</div>
 	);
 });
