aa51bc8f4a5b88071d642b6afac72de866624899
Kononkov1998 <kononkov98@mail.ru>
2021-04-15 23:40:24 +0700

    Добавил imdb_id в сериал

--- backend/shows/models.py
+++ backend/shows/models.py
@@ -7,6 +7,7 @@ from users.models import UserScore, UserLogAbstract
 
 
 class Show(models.Model):
+    imdb_id = models.CharField(max_length=20, blank=True)
     tmdb_id = models.IntegerField(unique=True)
     tmdb_original_name = models.CharField(max_length=200)
     tmdb_name = models.CharField(max_length=200)
