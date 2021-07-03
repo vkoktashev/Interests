8d350c5448c3bf578b2bc2520bad2ab5246644db
Vladislav <vladislav54415@gmail.com>
2021-07-03 23:53:18 +0700

    Собственные всплывающие окна

--- frontend/src/components/Modals/RegisterForm/RegisterForm.js
+++ frontend/src/components/Modals/RegisterForm/RegisterForm.js
@@ -2,9 +2,8 @@ import React, { useState } from "react";
 import { observer } from "mobx-react";
 import AuthStore from "../../../store/AuthStore";
 import PagesStore from "../../../store/PagesStore";
+import Modal from "../../Common/Modal/Modal";
 import "./register-form.sass";
-
-import { MDBModal, MDBModalBody, MDBContainer, MDBRow, MDBCol, MDBBtn } from "mdbreact";
 
 /**
  * КОмпонент формы авторизации
@@ -20,54 +19,46 @@ const RegisterForm = observer((props) => {
 	const [login, setLogin] = useState("");
 
 	return (
-		<MDBModal isOpen={RegistrateFormIsOpen} toggle={closeRegistrateForm} size='sm' centered>
-			<MDBModalBody className='register-form'>
-				<MDBContainer>
-					<MDBRow>
-						<MDBCol>
-							<form>
-								<p className='note note-danger register-form__fail' hidden={!registrateState.startsWith("error:")}>
-									{registrateState}
-								</p>
-								<p className='note note-success register-form__success' hidden={user?.email === ""}>
-									{user.login}, добро пожаловать! Осталось только подтвердить вашу почту
-								</p>
-								<h4>Регистрация</h4>
+		<Modal isOpen={RegistrateFormIsOpen} toggle={closeRegistrateForm} className='register-form'>
+			<form>
+				<p className='note note-danger register-form__fail' hidden={!registrateState.startsWith("error:")}>
+					{registrateState}
+				</p>
+				<p className='note note-success register-form__success' hidden={user?.email === ""}>
+					{user.login}, добро пожаловать! Осталось только подтвердить вашу почту
+				</p>
+				<h4>Регистрация</h4>
 
-								<label htmlFor='loginInput'>Никнейм</label>
-								<input type='text' id='loginInput' className='register-form__input' value={login} onChange={(event) => setLogin(event.target.value)} />
+				<label htmlFor='loginInput'>Никнейм</label>
+				<input type='text' id='loginInput' className='register-form__input' value={login} onChange={(event) => setLogin(event.target.value)} />
 
-								<label htmlFor='emailInput'>Электронная почта</label>
-								<input type='email' id='emailInput' className='register-form__input' value={email} onChange={(event) => setEmail(event.target.value)} />
+				<label htmlFor='emailInput'>Электронная почта</label>
+				<input type='email' id='emailInput' className='register-form__input' value={email} onChange={(event) => setEmail(event.target.value)} />
 
-								<label htmlFor='passwordInput'>Пароль</label>
-								<input type='password' id='passwordInput' className='register-form__input' value={password} onChange={(event) => setPassword(event.target.value)} />
+				<label htmlFor='passwordInput'>Пароль</label>
+				<input type='password' id='passwordInput' className='register-form__input' value={password} onChange={(event) => setPassword(event.target.value)} />
 
-								<label htmlFor='passwordConfirmInput'>Подтверждение пароля</label>
-								<input
-									type='password'
-									id='passwordConfirmInput'
-									className='register-form__input'
-									style={passwordConfirm === password ? { outlineColor: "green" } : { outlineColor: "red" }}
-									value={passwordConfirm}
-									onChange={(event) => setPasswordConfirm(event.target.value)}
-								/>
+				<label htmlFor='passwordConfirmInput'>Подтверждение пароля</label>
+				<input
+					type='password'
+					id='passwordConfirmInput'
+					className='register-form__input'
+					style={passwordConfirm === password ? { outlineColor: "green" } : { outlineColor: "red" }}
+					value={passwordConfirm}
+					onChange={(event) => setPasswordConfirm(event.target.value)}
+				/>
 
-								<div className='text-center mt-4'>
-									<MDBBtn
-										type='button'
-										className='register-form__auth-button'
-										disabled={password !== passwordConfirm || login.length < 1 || email.length < 1 || password.length < 1}
-										onClick={() => register(login, email, password)}>
-										{registrateState !== "pending" ? "Зарегистрироваться" : "Загрузка..."}
-									</MDBBtn>
-								</div>
-							</form>
-						</MDBCol>
-					</MDBRow>
-				</MDBContainer>
-			</MDBModalBody>
-		</MDBModal>
+				<div className='text-center mt-4'>
+					<button
+						type='button'
+						className='register-form__auth-button'
+						disabled={password !== passwordConfirm || login.length < 1 || email.length < 1 || password.length < 1}
+						onClick={() => register(login, email, password)}>
+						{registrateState !== "pending" ? "Зарегистрироваться" : "Загрузка..."}
+					</button>
+				</div>
+			</form>
+		</Modal>
 	);
 });
 
