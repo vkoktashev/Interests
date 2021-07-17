827069083c7dbc1135767bc28fba7b8b5292c20d
Vladislav <vladislav54415@gmail.com>
2021-07-17 12:11:13 +0700

    анимация модальных окон

--- frontend/src/components/Common/Modal/Modal.js
+++ frontend/src/components/Common/Modal/Modal.js
@@ -4,14 +4,12 @@ import classnames from "classnames";
 import "./modal.sass";
 
 function Modal({ className, children, isOpen, toggle }) {
-	if (isOpen) {
-		return (
-			<div hidden={!isOpen} className={classnames("modal2")}>
-				<div className={classnames("modal2__backdrop")} onClick={toggle} hidden={!isOpen} />
-				<div className={classnames("modal2__content", className)}>{children}</div>
-			</div>
-		);
-	} else return null;
+	return (
+		<div className={classnames("modal2", isOpen ? "modal2_open" : "")}>
+			<div className={classnames("modal2__backdrop")} onClick={toggle} />
+			<div className={classnames("modal2__content", className)}>{children}</div>
+		</div>
+	);
 }
 
 export default Modal;
