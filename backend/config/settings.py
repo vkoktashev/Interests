95c0e1e353d7de241cd5518fac21f38f33f6d0fd
Kononkov1998 <kononkov98@mail.ru>
2022-11-13 06:51:48 +0700

    hltb fix (upgrade version)

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -151,7 +151,6 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7)
 }
 
-# celery
 CELERY_BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
