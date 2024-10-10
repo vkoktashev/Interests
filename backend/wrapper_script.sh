3022a6fcc27b017229c8f8b144de82d922309aab
Evgeniy <38208432+Kononkov1998@users.noreply.github.com>
2024-10-10 21:56:58 +0700

    Update wrapper_script.sh

--- backend/wrapper_script.sh
+++ backend/wrapper_script.sh
@@ -1,3 +1,3 @@
 celery -A config worker -l info --logfile=/logs/celery_worker.log &
 celery -A config beat -l debug --logfile=/logs/celery_beat.log &
-gunicorn config.wsgi --timeout 60 --bind 0.0.0.0:8001
+gunicorn config.wsgi --timeout 5 --bind 0.0.0.0:8001
