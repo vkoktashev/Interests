95b9b0e507e753b8b8132c7122e732f649c31301
Evgeniy <kononkov98@mail.ru>
2024-12-25 22:25:04 +0300

    update manage.py

--- backend/manage.py
+++ backend/manage.py
@@ -2,9 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from dotenv import load_dotenv
 
-load_dotenv()
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
