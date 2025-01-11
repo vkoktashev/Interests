ed5ae85541e518ceb83e1f76cf520fd145a021a2
Evgeniy <kononkov98@mail.ru>
2025-01-11 17:06:29 +0300

    backend static

--- backend/wrapper_script.sh
+++ backend/wrapper_script.sh
@@ -1,3 +1,4 @@
 celery -A config worker -l info --logfile=/logs/celery_worker.log &
 celery -A config beat -l debug --logfile=/logs/celery_beat.log &
+python manage.py collectstatic --noinput
 uvicorn config.asgi:application --host 0.0.0.0 --port 8001 --workers 4
