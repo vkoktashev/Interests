d601f155686542bc43a903a8a03c5c76bbcb6196
Evgeniy <kononkov98@mail.ru>
2025-01-11 21:10:41 +0300

    static fix?

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -192,6 +192,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
