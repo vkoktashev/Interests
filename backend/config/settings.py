7fd4f5e403b0fc318a0eac7dea3473b339fe2899
Evgeniy <kononkov98@mail.ru>
2025-01-11 21:58:31 +0300

    static fix?

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -194,4 +194,4 @@ USE_TZ = True
 
 STATIC_URL = '/api/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
