70dabc795cb5c9edef64229725ae30e3b18150d2
Vladislav Koktashev <vladislav54415@gmail.com>
2026-02-21 01:22:20 +0300

    unwatchedPage update

--- frontend-new/src/routes/UnwatchedPage/views/EpisodeRow/EpisodeRow.tsx
+++ frontend-new/src/routes/UnwatchedPage/views/EpisodeRow/EpisodeRow.tsx
@@ -1,61 +1,89 @@
-import React from "react";
+import React from 'react';
 import {Link} from '@steroidsjs/core/ui/nav';
-import Rating from '../../../../shared/Rating';
-import "./episode-row.scss";
-import {ROUTE_SHOW_EPISODE} from '../../../index';
-import {CheckboxField} from '@steroidsjs/core/ui/form';
 import {useBem} from '@steroidsjs/core/hooks';
+import {CheckboxField} from '@steroidsjs/core/ui/form';
+import Rating from '../../../../shared/Rating';
+import {ROUTE_SHOW_EPISODE} from '../../../index';
+import {ISetEpisodesPayload, IUnwatchedEpisode} from '../types';
+import './episode-row.scss';
 
-function DetailedEpisodeRow({ episode, showID, seasonNumber, setShowEpisodeUserStatus, loggedIn }) {
-	const bem = useBem('DetailedEpisodeRow');
+interface IEpisodeRowProps {
+	episode: IUnwatchedEpisode;
+	showID: number;
+	seasonNumber: number;
+	setShowEpisodeUserStatus: (showId: string, payload: ISetEpisodesPayload) => Promise<void>;
+	loggedIn: boolean;
+}
 
-	function parseDate(date) {
-		let newDate = new Date(date);
-		return newDate.toLocaleDateString("ru-RU");
+function formatDate(date?: string): string {
+	if (!date) {
+		return 'Дата неизвестна';
 	}
+	const parsedDate = new Date(date);
+	if (Number.isNaN(parsedDate.getTime())) {
+		return 'Дата неизвестна';
+	}
+	return parsedDate.toLocaleDateString('ru-RU');
+}
+
+function EpisodeRow({
+	episode,
+	showID,
+	seasonNumber,
+	setShowEpisodeUserStatus,
+	loggedIn,
+}: IEpisodeRowProps) {
+	const bem = useBem('episode-row');
 
 	return (
 		<div className={bem.block()}>
-			<CheckboxField
-				fieldLayoutClassName={bem.element('checkbox')}
-				attribute={[showID, episode.tmdb_id].join('_')}
-			/>
-			<p className={bem.element('date')}>
-				{parseDate(episode.tmdb_release_date)}
-			</p>
-			<div hidden={!loggedIn} className={bem.element('rate')}>
-				<Rating
-					withEye={true}
-					readonly={!loggedIn}
-					initialRating={-1}
-					onChange={(score) => {
-						setShowEpisodeUserStatus(
-							showID,
-							{
-								episodes: [
-									{
-										tmdb_id: episode.tmdb_id,
-										score: score,
-									},
-								],
-							},
-						);
-					}}
+			<div className={bem.element('left')}>
+				<CheckboxField
+					fieldLayoutClassName={bem.element('checkbox')}
+					attribute={[showID, episode.tmdb_id].join('_')}
 				/>
+				<div className={bem.element('meta')}>
+					<span className={bem.element('code')}>
+						S{seasonNumber}E{episode.tmdb_episode_number}
+					</span>
+					<span className={bem.element('date')}>{formatDate(episode.tmdb_release_date)}</span>
+				</div>
 			</div>
-			<Link
-				className={bem.element('name')}
-				toRoute={ROUTE_SHOW_EPISODE}
-				toRouteParams={{
-					showId: showID,
-					showSeasonId: seasonNumber,
-					showEpisodeId: episode.tmdb_episode_number,
-				}}
-				title={episode.tmdb_name}>
-				Серия {episode.tmdb_episode_number} - {episode.tmdb_name}
-			</Link>
+
+			<div className={bem.element('center')}>
+				<Link
+					className={bem.element('name')}
+					toRoute={ROUTE_SHOW_EPISODE}
+					toRouteParams={{
+						showId: showID,
+						showSeasonId: seasonNumber,
+						showEpisodeId: episode.tmdb_episode_number,
+					}}
+					title={episode.tmdb_name}
+				>
+					{episode.tmdb_name || `Серия ${episode.tmdb_episode_number}`}
+				</Link>
+			</div>
+
+			{loggedIn && (
+				<div className={bem.element('rate')}>
+					<Rating
+						withEye
+						readonly={!loggedIn}
+						initialRating={-1}
+						onChange={score => {
+							setShowEpisodeUserStatus(String(showID), {
+								episodes: [{
+									tmdb_id: episode.tmdb_id,
+									score,
+								}],
+							});
+						}}
+					/>
+				</div>
+			)}
 		</div>
 	);
 }
 
-export default DetailedEpisodeRow;
+export default EpisodeRow;
