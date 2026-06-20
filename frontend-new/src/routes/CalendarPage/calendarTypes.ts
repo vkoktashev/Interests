741f06a008f3dcae0a336109727381687c23e0cb
Vladislav Koktashev <vladislav54415@gmail.com>
2026-06-20 12:06:43 +0300

    releases list update

--- frontend-new/src/routes/CalendarPage/calendarTypes.ts
+++ frontend-new/src/routes/CalendarPage/calendarTypes.ts
@@ -2,20 +2,24 @@ export interface ICalendarGame {
 	id: number;
 	name: string;
 	slug: string;
+	poster_path?: string;
 }
 
 export interface ICalendarMovie {
 	tmdb_id: number;
 	tmdb_name: string;
+	tmdb_poster_path?: string;
 }
 
 export interface ICalendarEpisode {
 	tmdb_id: number;
+	tmdb_name: string;
 	tmdb_season_number: number;
 	tmdb_episode_number: number;
 	tmdb_show: {
 		tmdb_id: number;
 		tmdb_name: string;
+		tmdb_poster_path?: string;
 	};
 }
 
