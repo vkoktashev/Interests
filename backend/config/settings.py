4faa34b41ba0bc54a865934cfba5237a95b4f074
Evgeniy Kononkov <kononkov98@mail.ru>
2026-07-04 16:15:08 +0300

    Use Redis cache

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -199,6 +199,14 @@ CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
 
+CACHE_URL = os.environ.get('CACHE_URL', os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379/1'))
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': CACHE_URL,
+    },
+}
+
 LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
